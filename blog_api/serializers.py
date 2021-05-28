618249235f2c6d42a8b262ba2564f9a20f37d52b
jimrahman <jimrahman.1994@gmail.com>
2021-05-28 02:38:54 -0400

    blogpost serializer created

--- blog_api/serializers.py
+++ blog_api/serializers.py
@@ -1,8 +1,19 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from blog_api.models import BlogPost
 
 
 class UserSerializer(serializers.ModelSerializer):
+    owner = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+
     class Meta:
         model = User
-        fields = ['id', 'username']
+        fields = ['id', 'username', 'posts']
+
+
+class BlogPostSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = BlogPost
+        fields = ['id', 'title', 'body', 'owner']
--- blog_api/views.py
+++ blog_api/views.py
@@ -1,9 +1,23 @@
 from rest_framework import generics
 from blog_api import serializers
 from django.contrib.auth.models import User
+from blog_api.models import BlogPost
 
 
 # Create your views here.
+class BlogPostList(generics.ListCreateAPIView):
+    queryset = BlogPost.objects.all()
+    serializer_class = serializers.BlogPostSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class BlogPostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = BlogPost.objects.all()
+    serializer_class = serializers.BlogPostSerializer
+
+
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = serializers.UserSerializer
